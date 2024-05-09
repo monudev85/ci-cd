7895b9f70d207936cff6f63c8b2ff5d5b5b77dc1
monudev85 <monu@knobee.app>
2024-05-09 15:47:11 +0530

    changed in the metor.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,5 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
-
+// const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+import { getDefaultConfig, mergeConfig } from "@react-native/metro-config";
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
 
@@ -11,12 +11,12 @@ const { assetExts, sourceExts } = defaultConfig.resolver;
  */
 const config = {
   transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   },
   resolver: {
     assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"]
-  }
+    sourceExts: [...sourceExts, "svg"],
+  },
 };
 
 module.exports = mergeConfig(defaultConfig, config);

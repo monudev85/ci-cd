c165f89f6db26a05537e657c2750c42c99052c64
monudev85 <monu@knobee.app>
2024-05-09 15:37:14 +0530

    worked on metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,12 @@
-const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
+const { getDefaultConfig } = require('@react-native/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
-const { assetExts, sourceExts } = defaultConfig.resolver;
-
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {
-  transformer: {
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
-  },
-  resolver: {
-    assetExts: assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...sourceExts, "svg"]
-  }
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  return {
+    ...defaultConfig,
+    transformer: {
+      ...defaultConfig.transformer,
+      routerRoot: 'path/to/routes/directory',
+    },
+  };
+})();
